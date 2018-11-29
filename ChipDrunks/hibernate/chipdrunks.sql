2661a8d7fa07356c363d2636cf128ed792d3c834
CRISTINAJAVAAWS <44088001+CRISTINAJAVAAWS@users.noreply.github.com>
2018-11-29 10:55:11 +0100

    chippies maven project

---
+++ ChipDrunks/hibernate/chipdrunks.sql
@@ -0,0 +1,10 @@
+CREATE SCHEMA `chipdrunks` ;
+
+USE mysql;
+
+CREATE USER 'chipdrunk_user'@'localhost' IDENTIFIED BY 'chipies';
+GRANT ALL PRIVILEGES ON chipdrunks.* TO 'chipdrunks_user'@'localhost';
+FLUSH PRIVILEGES;
+
+USE chipdrunks;
+
